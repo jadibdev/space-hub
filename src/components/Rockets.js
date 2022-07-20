81cea21a862ac2a8092c67cf4af9e61895442dbd
yohannesdagnachew <102184682+yohannesdagnachew@users.noreply.github.com>
2022-07-21 00:01:16 +0300

    Fix linter

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -6,18 +6,7 @@ import './Rockets.css';
 import RocketRender from './rocketRender';
 
 const Rockets = () => {
-  // const dispatch = useDispatch();
-  // const fetchData = async () => {
-  //   const res = await axios.get('https://api.spacexdata.com/v3/rockets')
-  //     .catch((err) => console.log('error', err));
-  //   const list = res.data;
-  //   dispatch(setData(list));
-  // };
-  // useEffect(() => {
-  //   fetchData();
-  // }, [dispatch]);
   const rock = useSelector((state) => state.rockets);
-  console.log(rock);
   return (
     rock.map((item) => (<RocketRender key={item.id} List={item} />))
   );
